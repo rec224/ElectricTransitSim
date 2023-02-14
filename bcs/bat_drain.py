00d2e94765c2a6fce5b9fa1ba204ebc06b6ec211
rec224 <rec224@lehigh.edu>
2023-02-14 15:59:27 -0500

    random route

--- bcs/bat_drain.py
+++ bcs/bat_drain.py
@@ -2,11 +2,14 @@
 #want the simulation to predict battery charge based on what a bus leaves with
 #and given a particular route
 import simpy
-def bus(env, name, bcs, route_time, charge_duration, battery):
-    #simulate driving to the BCS
-    yield env.timeout(route_time)
+import random
+routes = [1,2,3]
+r_times = [20, 25, 30]
+def bus(env, name, battery):
+    route = random.randint(1,3)
+    r_time = r_times[route]
     # battery will drain -- find out a way for it to vary
-    battery = battery - 15
+    battery = battery - r_time
     #request charging spot
     print('%s arriving at %d' % (name, env.now))
     with bcs.request() as req:
