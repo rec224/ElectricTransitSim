afefddd851db5c7f40ede7cdd7a63491fef109f1
rec224 <rec224@lehigh.edu>
2023-02-14 16:09:08 -0500

    random route

--- bcs/bat_drain.py
+++ bcs/bat_drain.py
@@ -5,30 +5,25 @@ import simpy
 import random
 routes = [1,2,3]
 r_times = [20, 25, 30]
+battery = 100
 def bus(env, name, battery):
     route = random.randint(1,3)
-    r_time = r_times[route]
+    r_time = r_times[route-1]
     # battery will drain -- find out a way for it to vary
     battery = battery - r_time
-    #request charging spot
-    print('%s arriving at %d' % (name, env.now))
-    with bcs.request() as req:
-        yield req
+    print('Bus battery on route  returning with %d percent battery' %battery)
 
-        #charge battery
-        print('%s starting to charge at %s' % (name, env.now))
-        yield env.timeout(charge_duration)
-        battery = 100
-        print('%s leaving the bcs at %s' % (name, env.now))
 
 env = simpy.Environment()
 # capacity limites the number of charging spots to 6 -- how many are there actually
 # resource limits are not important right now
-bcs = simpy.Resource(env, capacity=6)
+# bcs = simpy.Resource(env, capacity=6)
 # accurate bus numbers
 for i in range(0,5):
     #lets say buses have to charge for 3 hrs
     #want battery recorded too? 
-    env.process(bus(env, 'Bus %d' % i, bcs, i, 2, i+5))
+    bus(env, 'Bus %d' % i, battery)
+
+
 
 env.run()
