52ced380582b4b3993d332b3c7e8942a8c7b9157
rec224 <rec224@lehigh.edu>
2023-02-21 15:34:36 -0500

    replacing block of code

--- bcs/bat_drain.py
+++ bcs/bat_drain.py
@@ -10,42 +10,46 @@ from numpy.random import normal
 import matplotlib.pyplot as plt
 routes = [1,2,3]
 r_times = [20, 25, 30]
-battery = 100
+
 # what do we want the standard deviation of the distribution to be her
 # use scipy.stats.norm instead? 
 # is sample size based on number of buses or number of routes or bus drivers etc.?
 mean = 2
 std = 0.005
 energy_cons_dst = normal(2, 0.005, 50)
-# ^ creates normal dist w median 2, std dev 0.005, and sample size 10
-# create a histrogram to show the normal distribution
+# ^ creates normal dist w median 2, std dev 0.005, and sample size 50
+# create a histrogram to show the normal distribution (10 bins)
 count, bins, ignored = plt.hist(energy_cons_dst, 10)
 plt.show()
 
-# select random number of miles from range of options
-miles = random.randint(50, 150)
-
-
-# select random value from the probability distribution
-# needs some sort of division or percentage function, 
-# otherwise, battery percentage ends up negative
-energy_cons_val = random.normalvariate(mean, std)/3
-#calculate the random amount of energy used 
-energy = miles * energy_cons_val
 def bus(env, name, battery):
     # battery will drain -- find out a way for it to vary
+    # select random number of miles from range of options
+    miles = random.randint(50, 150)
+    # select random value from the probability distribution
+    # needs some sort of division or percentage function, 
+    # otherwise, battery percentage ends up negative
+    energy_cons_val = random.normalvariate(mean, std)/3
+    #calculate the random amount of energy used 
+    energy = miles * energy_cons_val
     battery = battery - energy
-    print('Bus on route  returning with %d percent battery' %battery)
+    print('Bus on route %d returning with %d percent battery' %(i , battery))
+
 env = simpy.Environment()
+# can ignore anything below here, this stuff will be useful later: 
+
+
 # capacity limites the number of charging spots to 6 -- how many are there actually
 # resource limits are not important right now
 # bcs = simpy.Resource(env, capacity=6)
 # accurate bus numbers
+
+#running the file for 5 buses (five times)
 for i in range(0,5):
     #lets say buses have to charge for 3 hrs
     #want battery recorded too? 
+    battery = 100
     bus(env, 'Bus %d' % i, battery)
-
 
 
 env.run()
