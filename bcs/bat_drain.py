8c62c6c9cebd772e38723da59198a840d2483783
rec224 <rec224@lehigh.edu>
2023-02-21 16:07:55 -0500

    scheduling and battery levels

--- bcs/bat_drain.py
+++ bcs/bat_drain.py
@@ -32,7 +32,7 @@ def bus(env, name, battery):
     #calculate the random amount of energy used 
     energy = miles * energy_cons_val
     battery = battery - energy
-    print('Bus on route %d returning with %d percent battery' %(i , battery))
+    print('Bus %d returning with %d percent battery' %(i , battery))
 
 env = simpy.Environment()
 # can ignore anything below here, this stuff will be useful later: 
@@ -44,11 +44,24 @@ env = simpy.Environment()
 # accurate bus numbers
 
 #running the file for 5 buses (five times)
+#schedule for the buses starts at time 0, expected route duration is 2hrs (120 min)
+scheduled_start = 0
 for i in range(1,6):
     #lets say buses have to charge for 3 hrs
     #want battery recorded too? 
+    #bus leaves at a cheduled time (everry 120 minutes)
+    print('Bus %d leaving at time %d' % (i, scheduled_start))
     battery = 100
+    #bus drives its route
     bus(env, 'Bus %d' % i, battery)
+    #the duration of the drive will vary, lets call average of 120 minutes and std of 45 minutes
+    drive_dur = random.normalvariate(120, 45)
+    #calculate return time
+    print('Bus %d returning at time %d' % (i, scheduled_start + drive_dur))
+    print('Expected return %d' %(scheduled_start + 120))
+    #increment the scheduled start time for the next bus
+    scheduled_start = scheduled_start + 120
+    print('\n')
 
 
 env.run()
