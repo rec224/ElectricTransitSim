1bed7371c348268bd52e110b572d9206f60ab3dd
mlc325 <mlc325@lehigh.edu>
2023-02-23 16:05:08 -0500

    lots of charts

--- data/animation2.py
+++ data/animation2.py
@@ -7,9 +7,9 @@ arr = df.to_numpy()
 
 colorArr = ["red", "green", "blue", "orange", "purple", "skyblue", "hotpink", "teal", "lime", "navy", "goldenrod", "plum", "firebrick", "silver"]
 
-for filter in range(1, 50, 2):
+for filter in range(0, 51, 1):
   for i in range (0, 157, 12):
-      x = arr[:,i+2] #speed
+      x = arr[:,i+10] #distance
       y = arr[:,i+4] #energy
       
       filter_arr = []
@@ -25,12 +25,12 @@ for filter in range(1, 50, 2):
       l = str(2600+int(i/12))
       plt.scatter(newX,newY, 3, color=colorArr[int(i/12)], label = l)
 
-  plt.xlabel("Average speed (mph)")
+  plt.xlabel("Distance (miles)")
   plt.ylabel("Energy used (kWh)")
-  plt.title("Energy (kWh) vs speed (mph) driving for at least " + str(filter) + " percent of the hour")
+  plt.title("Energy (kWh) vs distance (miles) driving for at least " + str(filter) + " percent of the hour")
   # leg = plt.legend(loc ="upper right");
-  filename = "animation_chartsv2/chart" + str(filter)
-  plt.xlim(0,150)
-  plt.ylim(0,65)
+  filename = "animation_chartsv3/chart" + str(filter)
+#   plt.xlim(0,150)
+#   plt.ylim(0,65)
   plt.savefig(filename)
   plt.show()
