2afc20ab8ce050f8b87c97c75b75a504aa89d34f
rec224 <rec224@lehigh.edu>
2023-03-30 16:00:20 -0400

    finding num of unique drivers

---
+++ bcs/driver_analysis.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy
+from fitter import Fitter, get_distributions, get_common_distributions
+dataframe1 = pd.read_excel('driver_data_points.xlsx')
+print(dataframe1)
+#now we have to go through and group the data by driver ids
+i =0
+driver_ids =[]
+while i<539:
+    driver_ids.append(dataframe1.iloc[i]['op_id'])
+    i=i+1
+u_driver_ids = numpy.unique(driver_ids)
+print(u_driver_ids)
+
+k=len(u_driver_ids)
+class Driver:
+    def __init__(self, op_id, num_drives, kwh) :
+        self.op_id = op_id
+        self.num_drives = num_drives
+        self.kwh = kwh
+
+driver_list = []
+j=0
+while j<k:
+    n = driver_ids.count(u_driver_ids[j])
+    driver_list.append(Driver(u_driver_ids[j], n, 0))
+    print('Driver_id %s w/ num_drives %s and kwh %d' % (u_driver_ids[j], n, 0))
+    j=j+1   
+
+
