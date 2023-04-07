54ed2aa71dd2c34bb5a5fd035e5e507ec6437056
rec224 <rec224@lehigh.edu>
2023-04-07 12:43:34 -0400

    created driver analysis figure

--- bcs/driver_analysis.py
+++ bcs/driver_analysis.py
@@ -1,5 +1,7 @@
+from matplotlib import patches
 import pandas as pd
 import numpy
+import matplotlib.pyplot as plt
 from fitter import Fitter, get_distributions, get_common_distributions
 dataframe1 = pd.read_excel('driver_data_points.xlsx')
 #print(dataframe1)
@@ -11,28 +13,18 @@ while i<539:
     i=i+1
 u_driver_ids = numpy.unique(driver_ids)
 print(u_driver_ids)
-
-k=len(u_driver_ids)
-class Driver:
-    def __init__(self, op_id, num_drives, kwh) :
-        self.op_id = op_id
-        self.num_drives = num_drives
-        self.kwh = kwh
-
-driver_list = []
-j=0
-while j<k:
-    n = driver_ids.count(u_driver_ids[j])
-    driver_list.append(Driver(u_driver_ids[j], n, 0))
-    #print('Driver_id %s w/ num_drives %s and kwh %d' % (u_driver_ids[j], n, 0))
-    j=j+1   
-
-kwh_sum_per_d=[]
+kwh_avg_per_d=[]
 z =0
-while z<1:
+while z<k:
     df2=dataframe1.loc[(dataframe1['op_id'] == u_driver_ids[z])]
-    y = len(df2)
-    print(y)
+    n = df2['kwh'].mean()
+    kwh_avg_per_d.append(n)
     z=z+1
-
+print(kwh_avg_per_d)
+fig, ax = plt.subplots()
+ax.plot(u_driver_ids, kwh_avg_per_d, 'ro')
+plt.title('Mean kwh for each unique driver')
+plt.xlabel('Driver')
+plt.ylabel('kwh')
+plt.show()
 #print(df2)
